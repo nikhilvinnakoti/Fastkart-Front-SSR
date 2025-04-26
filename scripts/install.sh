b2b0e205882a35f9c84d9530cc30f2295ea58d9e
nikhilvinnakoti <64284901+nikhilvinnakoti@users.noreply.github.com>
2025-04-26 16:30:58 -0500

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo "Frontend installed. No extra action needed if NGINX is set up."
+
+# Go to frontend folder
+cd /home/ec2-user/fastkart/frontend
+
+# Install node_modules if needed (optional for frontend if only SSR server needs)
+npm install
