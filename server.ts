25ee9d55fa9008b44b89a57b59b9396ef7e06c1d
nikhilvinnakoti <64284901+nikhilvinnakoti@users.noreply.github.com>
2025-05-01 18:28:31 -0500

    Update server.ts

--- server.ts
+++ server.ts
@@ -20,26 +20,31 @@ export function app(): express.Express {
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
-  server.get('**', express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: 'index.html',
+  //server.get('**', express.static(browserDistFolder, {
+    //maxAge: '1y',
+   // index: 'index.html',
+ // }));
+  server.use(express.static(browserDistFolder, {
+  maxAge: '1y',
   }));
 
   // All regular routes use the Angular engine
-  server.get('**', (req, res, next) => {
-    const { protocol, originalUrl, baseUrl, headers } = req;
+  // All regular routes use the Angular engine
+server.get('*', (req, res, next) => {
+  const { protocol, originalUrl, baseUrl, headers } = req;
 
-    commonEngine
-      .render({
-        bootstrap,
-        documentFilePath: indexHtml,
-        url: `${protocol}://${headers.host}${originalUrl}`,
-        publicPath: browserDistFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-      })
-      .then((html) => res.send(html))
-      .catch((err) => next(err));
-  });
+  commonEngine
+    .render({
+      bootstrap,
+      documentFilePath: indexHtml,
+      url: `${protocol}://${headers.host}${originalUrl}`,
+      publicPath: browserDistFolder,
+      providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+    })
+    .then((html) => res.send(html))
+    .catch((err) => next(err));
+});
+
 
   return server;
 }
